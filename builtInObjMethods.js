18083c2128d9abf458f81fe39e311cd692ec679c
Eroje <93765911+Eroje@users.noreply.github.com>
2023-08-04 14:53:58 +0100

    fourth Commit

--- builtInObjMethods.js
+++ builtInObjMethods.js
@@ -6,18 +6,18 @@ const robot = {
   energyLevel: 75
 };
 
+// Object.keys() Built-in Method
 // Saves and return the robot keys (property names) in an array 
-// Object.keys() --- Method
 const robotKeys = Object.keys(robot);
 console.log(robotKeys);
 
+// Object.entries() Built-in Method
 // Saves and return the robot keys-value pair in an array
-// Object.entries() --- Method
 const robotEntries = Object.entries(robot);
 console.log(robotEntries);
 
+//Ogject.assign() Built-in Method
 // Creates a new robot with aditional properties
-//Ogject.assign() --- Method
 const newRobot = Object.assign({laserBlaster: true, voiceRecognition: true}, robot);
 
 console.log(newRobot);
